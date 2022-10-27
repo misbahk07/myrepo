20d2f1d39d461c83cd3e793c7aa5a8a4fe001209
misbahk07 <khanmisbahh777@gmail.com>
2022-10-27 18:07:26 +0530

    modified

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 
-const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    category:String,
-    year:Number
-    
-}, { timestamps: true });
-
+const bookSchema=new mongoose.Schema ({
+    name:String,
+    authorId:{
+        type:Number,
+        required:true
+    },
+    price:Number,
+    ratings:Number
+},{timestamps:true})
 
 module.exports = mongoose.model('Book', bookSchema) //users
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,17 +3,77 @@ const router = express.Router();
 // const UserModel= require("../models/userModel.js")
 const UserController= require("../controllers/userController")
 const BookController= require("../controllers/bookController")
+const AuthorController=require("../controllers/authorController")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
+router.post("/author",AuthorController.createAuthor)
 
-router.get("/getUsersData", UserController.getUsersData)
+router.post("/books",BookController.createBooks)
 
-router.post("/createBook", BookController.createBook  )
+router.get("/getAllBooks",AuthorController.getAllBooks)
 
-router.get("/getBooksData", BookController.getBooksData)
+router.get("/getTheAuthor",AuthorController.getTheAuthor)
+
+
+
+
+
+// router.post("/createUser", UserController.createUser  )
+
+// router.get("/getUsersData", UserController.getUsersData)
+
+// router.post("/createBook", BookController.createBook  )
+
+// router.get("/getBooksData", BookController.getBooksData)
+
+// query Params
+
+// let persons= [
+//     {
+//     name: "PK",
+//     age: 10,
+//     votingStatus: false
+//  },
+//  {
+//     name: "SK",
+//     age: 20,
+//     votingStatus: false
+//  },
+//  {
+//     name: "AA",
+//     age: 70,
+//     votingStatus: false
+//  },
+//  {
+//     name: "SC",
+//     age: 5,
+//     votingStatus: false
+//  },
+//  {
+//     name: "HO",
+//     age: 40,
+//     votingStatus: false
+//  }
+//  ]
+
+//  router.post("/persons1",function(req,res){
+    
+//     let voterAge=req.query.voterAge
+//     let updatedPersons=[]
+//     for(let i=0;i<persons.length;i++){
+//         if(persons[i].age>=voterAge){
+//             persons[i].votingStatus=true
+//             updatedPersons.push(persons)
+//         }
+//     }
+//     res.send({data :persons})
+        
+//  })
+
+
+
 
 module.exports = router;  
