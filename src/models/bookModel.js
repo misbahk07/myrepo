d8d45f299cf7cc892b4afb1c04fdcdb8e6952f1a
misbahk07 <115547055+misbahk07@users.noreply.github.com>
2022-10-31 22:16:07 +0530

    Update bookModel.js

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -11,16 +11,4 @@ const bookSchema = new mongoose.Schema( {
 
 module.exports = mongoose.model('Book', bookSchema) //users
 
-//Validation:
-//require:true
-//unique
-// default
 
-//String
-//Number
-//Date
-//Boolean
-// Arrays
-// Object
-// ObjectId
-// Buffer - not cover
